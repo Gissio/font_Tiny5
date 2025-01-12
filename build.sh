1fe86ff4ba6504e67ed0b508a1c09f866e088be9
Gissio <mressl@umich.edu>
2025-01-12 18:07:42 +0000

    2.001

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Variables
-COMMON=('-v' '--designer' 'Stefan Schmidt' '--designer-url' 'https://github.com/Gissio/font_tiny5' '--manufacturer' 'Stefan Schmidt Art' '--manufacturer-url' 'https://www.stefanschmidtart.com/' '--license' 'This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://openfontlicense.org' '--license-url' 'https://openfontlicense.org' '--strikeout-position' '2' '--strikeout-thickness' '1' '--underline-position' '-1' '--underline-thickness' '1' '--notdef-codepoint' '0x0' '--glyph-offset-x' '0.5' '--variable-axes' 'ESIZ,ROND,BLED' --variable-instance 'Solid,ESIZ=1,ROND=0,BLED=0' --variable-instance 'LCD,ESIZ=0.85,ROND=0,BLED=0' --variable-instance 'CRT,ESIZ=0.7,ROND=0.8,BLED=0.64' --variable-instance 'Matrix,ESIZ=0.85,ROND=0.8,BLED=0' '--static-axes' 'ROND=1')
+COMMON=('-v' '--designer' 'Stefan Schmidt' '--designer-url' 'https://github.com/Gissio/font_tiny5' '--manufacturer' 'Stefan Schmidt Art' '--manufacturer-url' 'https://www.stefanschmidtart.com/' '--license' 'This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://openfontlicense.org' '--license-url' 'https://openfontlicense.org' '--strikeout-position' '2' '--strikeout-thickness' '1' '--underline-position' '-1' '--underline-thickness' '1' '--notdef-codepoint' '0x0' '--glyph-offset-x' '0.5' '--variable-axes' 'ESIZ,ROND,BLED' --variable-instance 'Solid,ESIZ=1,ROND=0,BLED=0' --variable-instance 'LCD,ESIZ=0.85,ROND=0,BLED=0' --variable-instance 'CRT,ESIZ=0.7,ROND=0.8,BLED=0.64' --variable-instance 'Matrix,ESIZ=0.85,ROND=0.8,BLED=0')
 
 # Build Tiny5 Regular
 python scripts/bdf2ufo.py "${COMMON[@]}" sources/Tiny5-Regular.bdf build
